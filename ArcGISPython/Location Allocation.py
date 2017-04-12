fef17b67c4c7c0c9afe53f84cb4ddc9909d48544
Ben Wagner <bw2013@live.com>
2017-04-12 09:53:01 -0500

    Add ability to read user input from scrip t tool.

--- ArcGISPython/Location Allocation.py
+++ ArcGISPython/Location Allocation.py
@@ -2,13 +2,16 @@
 import arcpy
 
 def LoadFacilities():
-
-    arcpy.SelectLayerByAttribute_management("BG15_Tiger_NE_SD_BA_Data_PNT", "NEW_SELECTION", ' "FacType_NE" = 0 ')
+    
 
     # Local variables:
     Location_Allocation = "Location-Allocation"
     Location_Allocation__2_ = Location_Allocation
-    BG15_Tiger_NE_SD_BA_Data_PNT = "BG15_Tiger_NE_SD_BA_Data_PNT"
+    #Having issues with this, check data type
+    BG15_Tiger_NE_SD_BA_Data_PNT = arcpy.GetParameterAsText(0)
+    
+
+    arcpy.SelectLayerByAttribute_management(arcpy.MakeTableView_management(BG15_Tiger_NE_SD_BA_Data_PNT, "Table"), "NEW_SELECTION", ' "FacType_NE" = 0 ')
 
     # Process: Add Locations
     arcpy.AddLocations_na(Location_Allocation, "Facilities", BG15_Tiger_NE_SD_BA_Data_PNT, "Name GEOID #;FacilityType FacType_NE #", "5 Miles", "", "NE_SD_Roads2016 SHAPE;Roads_ND_Junctions NONE", "MATCH_TO_CLOSEST", "APPEND", "NO_SNAP", "5 Meters", "INCLUDE", "NE_SD_Roads2016 #;Roads_ND_Junctions #")
@@ -30,7 +33,8 @@ def LoadDemandPoints():
 def MakeLocationAllocationLayer():
 
     # Local variables:
-    Roads_ND = "Roads_ND"
+    #Roads_ND = "Roads_ND"
+    Roads_ND = arcpy.GetParameterAsText(1);
     Location_Allocation = "Location-Allocation"
 
     # Process: Make Location-Allocation Layer
