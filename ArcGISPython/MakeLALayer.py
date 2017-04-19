a977cadbfa47b34303dd393e506e486b617d738d
Ben Wagner <bw2013@live.com>
2017-04-19 10:02:24 -0500

    Add input parameters

--- ArcGISPython/MakeLALayer.py
+++ ArcGISPython/MakeLALayer.py
@@ -1,11 +1,19 @@
 # Import arcpy module
+
 import arcpy
 
 
+
 # Local variables:
-Roads_ND = "Roads_ND"
+
+
+Roads_ND = arcpy.GetParameterAsText(0)
+
+
 Location_Allocation = "Location-Allocation"
 
+
 # Process: Make Location-Allocation Layer
+
 arcpy.MakeLocationAllocationLayer_na(Roads_ND, "Location-Allocation", "Minutes", "FACILITY_TO_DEMAND", "MINIMIZE_IMPEDANCE", "1", "", "LINEAR", "1", "10", "", "ALLOW_DEAD_ENDS_AND_INTERSECTIONS_ONLY", "", "NO_HIERARCHY", "STRAIGHT_LINES", "1", "")
 
